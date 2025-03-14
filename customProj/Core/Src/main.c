d9b498604112714d60d99d2e21d552a9295eac49
ttm-SP <s.prabhudev@ttmrail.com.au>
2025-03-14 15:13:08 +1100

    FORMAT - On save

--- customProj/Core/Src/main.c
+++ customProj/Core/Src/main.c
@@ -15,8 +15,8 @@
   *
   ******************************************************************************
   */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
+  /* USER CODE END Header */
+  /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -46,10 +46,10 @@
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
-void SystemClock_Config(void);
-void PeriphCommonClock_Config(void);
-static void MX_GPIO_Init(void);
-static void MX_USART1_UART_Init(void);
+void SystemClock_Config( void );
+void PeriphCommonClock_Config( void );
+static void MX_GPIO_Init( void );
+static void MX_USART1_UART_Init( void );
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -63,7 +63,7 @@ static void MX_USART1_UART_Init(void);
   * @brief  The application entry point.
   * @retval int
   */
-int main(void)
+int main( void )
 {
 
   /* USER CODE BEGIN 1 */
@@ -75,10 +75,10 @@ int main(void)
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
 
   /* System interrupt init*/
-  NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+  NVIC_SetPriorityGrouping( NVIC_PRIORITYGROUP_4 );
 
   /* SysTick_IRQn interrupt configuration */
-  NVIC_SetPriority(SysTick_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),15, 0));
+  NVIC_SetPriority( SysTick_IRQn, NVIC_EncodePriority( NVIC_GetPriorityGrouping(), 15, 0 ) );
 
   /* USER CODE BEGIN Init */
 
@@ -103,7 +103,7 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
+  while ( 1 )
   {
     /* USER CODE END WHILE */
 
@@ -116,69 +116,69 @@ int main(void)
   * @brief System Clock Configuration
   * @retval None
   */
-void SystemClock_Config(void)
+void SystemClock_Config( void )
 {
-  LL_FLASH_SetLatency(LL_FLASH_LATENCY_3);
-  while(LL_FLASH_GetLatency() != LL_FLASH_LATENCY_3)
+  LL_FLASH_SetLatency( LL_FLASH_LATENCY_3 );
+  while ( LL_FLASH_GetLatency() != LL_FLASH_LATENCY_3 )
   {
   }
 
   /* HSE configuration and activation */
   LL_RCC_HSE_Enable();
-  while(LL_RCC_HSE_IsReady() != 1)
+  while ( LL_RCC_HSE_IsReady() != 1 )
   {
   }
 
   /* HSI configuration and activation */
   LL_RCC_HSI_Enable();
-  while(LL_RCC_HSI_IsReady() != 1)
+  while ( LL_RCC_HSI_IsReady() != 1 )
   {
   }
 
   /* Main PLL configuration and activation */
-  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI, LL_RCC_PLLM_DIV_1, 8, LL_RCC_PLLR_DIV_2);
+  LL_RCC_PLL_ConfigDomain_SYS( LL_RCC_PLLSOURCE_HSI, LL_RCC_PLLM_DIV_1, 8, LL_RCC_PLLR_DIV_2 );
   LL_RCC_PLL_Enable();
   LL_RCC_PLL_EnableDomain_SYS();
-  while(LL_RCC_PLL_IsReady() != 1)
+  while ( LL_RCC_PLL_IsReady() != 1 )
   {
   }
 
   /* Sysclk activation on the main PLL */
   /* Set CPU1 prescaler*/
-  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+  LL_RCC_SetAHBPrescaler( LL_RCC_SYSCLK_DIV_1 );
 
   /* Set CPU2 prescaler*/
-  LL_C2_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_2);
+  LL_C2_RCC_SetAHBPrescaler( LL_RCC_SYSCLK_DIV_2 );
 
-  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
-  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
+  LL_RCC_SetSysClkSource( LL_RCC_SYS_CLKSOURCE_PLL );
+  while ( LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL )
   {
   }
 
   /* Set AHB SHARED prescaler*/
-  LL_RCC_SetAHB4Prescaler(LL_RCC_SYSCLK_DIV_1);
+  LL_RCC_SetAHB4Prescaler( LL_RCC_SYSCLK_DIV_1 );
 
   /* Set APB1 prescaler*/
-  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+  LL_RCC_SetAPB1Prescaler( LL_RCC_APB1_DIV_1 );
 
   /* Set APB2 prescaler*/
-  LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
+  LL_RCC_SetAPB2Prescaler( LL_RCC_APB2_DIV_1 );
 
-  LL_Init1msTick(64000000);
+  LL_Init1msTick( 64000000 );
 
   /* Update CMSIS variable (which can be updated also through SystemCoreClockUpdate function) */
-  LL_SetSystemCoreClock(64000000);
+  LL_SetSystemCoreClock( 64000000 );
 }
 
 /**
   * @brief Peripherals Common Clock Configuration
   * @retval None
   */
-void PeriphCommonClock_Config(void)
+void PeriphCommonClock_Config( void )
 {
-  LL_RCC_SetSMPSClockSource(LL_RCC_SMPS_CLKSOURCE_HSI);
-  LL_RCC_SetSMPSPrescaler(LL_RCC_SMPS_DIV_1);
-  LL_RCC_SetRFWKPClockSource(LL_RCC_RFWKP_CLKSOURCE_NONE);
+  LL_RCC_SetSMPSClockSource( LL_RCC_SMPS_CLKSOURCE_HSI );
+  LL_RCC_SetSMPSPrescaler( LL_RCC_SMPS_DIV_1 );
+  LL_RCC_SetRFWKPClockSource( LL_RCC_RFWKP_CLKSOURCE_NONE );
   /* USER CODE BEGIN Smps */
 
   /* USER CODE END Smps */
@@ -189,34 +189,34 @@ void PeriphCommonClock_Config(void)
   * @param None
   * @retval None
   */
-static void MX_USART1_UART_Init(void)
+static void MX_USART1_UART_Init( void )
 {
 
   /* USER CODE BEGIN USART1_Init 0 */
 
   /* USER CODE END USART1_Init 0 */
 
-  LL_USART_InitTypeDef USART_InitStruct = {0};
+  LL_USART_InitTypeDef USART_InitStruct = { 0 };
 
-  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+  LL_GPIO_InitTypeDef GPIO_InitStruct = { 0 };
 
-  LL_RCC_SetUSARTClockSource(LL_RCC_USART1_CLKSOURCE_PCLK2);
+  LL_RCC_SetUSARTClockSource( LL_RCC_USART1_CLKSOURCE_PCLK2 );
 
   /* Peripheral clock enable */
-  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_USART1);
+  LL_APB2_GRP1_EnableClock( LL_APB2_GRP1_PERIPH_USART1 );
 
-  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOB);
+  LL_AHB2_GRP1_EnableClock( LL_AHB2_GRP1_PERIPH_GPIOB );
   /**USART1 GPIO Configuration
   PB6   ------> USART1_TX
   PB7   ------> USART1_RX
   */
-  GPIO_InitStruct.Pin = LL_GPIO_PIN_6|LL_GPIO_PIN_7;
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_6 | LL_GPIO_PIN_7;
   GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
   GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
   GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
   GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
   GPIO_InitStruct.Alternate = LL_GPIO_AF_7;
-  LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  LL_GPIO_Init( GPIOB, &GPIO_InitStruct );
 
   /* USER CODE BEGIN USART1_Init 1 */
 
@@ -229,20 +229,20 @@ static void MX_USART1_UART_Init(void)
   USART_InitStruct.TransferDirection = LL_USART_DIRECTION_TX_RX;
   USART_InitStruct.HardwareFlowControl = LL_USART_HWCONTROL_NONE;
   USART_InitStruct.OverSampling = LL_USART_OVERSAMPLING_16;
-  LL_USART_Init(USART1, &USART_InitStruct);
-  LL_USART_SetTXFIFOThreshold(USART1, LL_USART_FIFOTHRESHOLD_1_8);
-  LL_USART_SetRXFIFOThreshold(USART1, LL_USART_FIFOTHRESHOLD_1_8);
-  LL_USART_DisableFIFO(USART1);
-  LL_USART_ConfigAsyncMode(USART1);
+  LL_USART_Init( USART1, &USART_InitStruct );
+  LL_USART_SetTXFIFOThreshold( USART1, LL_USART_FIFOTHRESHOLD_1_8 );
+  LL_USART_SetRXFIFOThreshold( USART1, LL_USART_FIFOTHRESHOLD_1_8 );
+  LL_USART_DisableFIFO( USART1 );
+  LL_USART_ConfigAsyncMode( USART1 );
 
   /* USER CODE BEGIN WKUPType USART1 */
 
   /* USER CODE END WKUPType USART1 */
 
-  LL_USART_Enable(USART1);
+  LL_USART_Enable( USART1 );
 
   /* Polling USART1 initialisation */
-  while((!(LL_USART_IsActiveFlag_TEACK(USART1))) || (!(LL_USART_IsActiveFlag_REACK(USART1))))
+  while ( ( !( LL_USART_IsActiveFlag_TEACK( USART1 ) ) ) || ( !( LL_USART_IsActiveFlag_REACK( USART1 ) ) ) )
   {
   }
   /* USER CODE BEGIN USART1_Init 2 */
@@ -256,18 +256,18 @@ static void MX_USART1_UART_Init(void)
   * @param None
   * @retval None
   */
-static void MX_GPIO_Init(void)
+static void MX_GPIO_Init( void )
 {
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+  /* USER CODE END MX_GPIO_Init_1 */
 
-  /* GPIO Ports Clock Enable */
-  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOC);
-  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOA);
-  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOB);
+    /* GPIO Ports Clock Enable */
+  LL_AHB2_GRP1_EnableClock( LL_AHB2_GRP1_PERIPH_GPIOC );
+  LL_AHB2_GRP1_EnableClock( LL_AHB2_GRP1_PERIPH_GPIOA );
+  LL_AHB2_GRP1_EnableClock( LL_AHB2_GRP1_PERIPH_GPIOB );
 
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+  /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
@@ -278,12 +278,12 @@ static void MX_GPIO_Init(void)
   * @brief  This function is executed in case of error occurrence.
   * @retval None
   */
-void Error_Handler(void)
+void Error_Handler( void )
 {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
-  while (1)
+  while ( 1 )
   {
   }
   /* USER CODE END Error_Handler_Debug */
@@ -297,11 +297,11 @@ void Error_Handler(void)
   * @param  line: assert_param error line source number
   * @retval None
   */
-void assert_failed(uint8_t *file, uint32_t line)
+void assert_failed( uint8_t * file, uint32_t line )
 {
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+     /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
